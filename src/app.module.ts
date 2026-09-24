5c6ade2f37dc0cb9a47627c5f9cab2822c3b52b8
Vladislav Skulachenko <vladik-skulahenko@yandex.ru>
2026-09-24 22:10:52 +0300

    feat(config): add JWT config (jwt.config)

--- src/app.module.ts
+++ src/app.module.ts
@@ -8,12 +8,13 @@ import { AuthModule } from './auth/auth.module';
 import { AppDataSource } from './ormconfig';
 import { dbConfig } from './config/db.config';
 import { appConfig } from './config/app.config';
+import { jwtConfig } from './config/jwt.config';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
-      load: [appConfig, dbConfig],
+      load: [appConfig, dbConfig, jwtConfig],
     }),
     TypeOrmModule.forRoot(AppDataSource.options),
     UsersModule,
